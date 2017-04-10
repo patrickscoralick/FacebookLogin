0daed5cd88008e9123b9d82f90eb342c4a82c876
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2017-04-10 12:26:06 -0300

    Upando projeto

---
+++ app/src/main/java/com/example/patrickcosta/layoutandroid_3003_luca_patrick_thais/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.patrickcosta.layoutandroid_3003_luca_patrick_thais;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
